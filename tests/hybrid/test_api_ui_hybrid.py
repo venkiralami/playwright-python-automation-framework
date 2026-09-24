1f65c6dad808f3e90aee81a2fe1e3f738b1fe3f4
venkiralami <venki.ralami@gmail.com>
2026-09-24 18:58:00 -0500

    To check Q gate for pull

--- tests/hybrid/test_api_ui_hybrid.py
+++ tests/hybrid/test_api_ui_hybrid.py
@@ -5,7 +5,7 @@ from playwright.sync_api import expect
 def test_api_setup_ui_validation(created_user, login_page):
     
     # -------------------------
-    # API SETUP
+    # API SETUP 
     # -------------------------
 
     print("\nUser created through API:", created_user)
